134578ba0ae00d4613484fa9e384b6837db688a8
Sheng <sisheng1998@gmail.com>
2024-05-01 13:07:29 +0800

    add routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use('*', cors())
+
+app.get('/', (c) => c.redirect('https://sisheng.my', 302))
+
+app.post('/send', (c) => c.json({ success: true, message: 'Email sent!' }))
+
+app.notFound((c) => c.json({ success: false, message: 'Not found' }, 404))
+
+app.onError((err, c) => c.json({ success: false, message: err.message }, 500))
 
 export default app
