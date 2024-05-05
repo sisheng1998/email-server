f24f4444e1a89636cf3cbdcc4369b99253bb7d57
Sheng <sisheng1998@gmail.com>
2024-05-05 08:52:32 +0800

    make subject required

--- src/zod.ts
+++ src/zod.ts
@@ -14,7 +14,7 @@ const emailSchema = z.object({
   cc: z.union([contactSchema, z.array(contactSchema)]).optional(),
   bcc: z.union([contactSchema, z.array(contactSchema)]).optional(),
   from: contactSchema,
-  subject: z.string(),
+  subject: z.string().trim().min(1, { message: 'Subject is required' }),
   text: z.string().optional(),
   html: z.string().optional(),
   dryRun: z.boolean().optional().default(false),
