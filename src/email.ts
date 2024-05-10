f15c589ef2ff2bf48ae86dad64d9600c67482446
Sheng <sisheng1998@gmail.com>
2024-05-10 21:05:21 +0800

    implement retry logic for failed attempts

--- src/email.ts
+++ src/email.ts
@@ -3,28 +3,55 @@ import { Bindings } from './types'
 import { Email } from './zod'
 import { getMCEmail } from './utils'
 
-export const sendEmail = async (email: Email, env: Bindings) => {
+export const sendEmail = async (
+  email: Email,
+  env: Bindings,
+  attempts: number = 3,
+  delay: number = 1000
+) => {
   const mcEmail = getMCEmail(email, env)
 
-  const response = await fetch(
-    `https://api.mailchannels.net/tx/v1/send${
-      email.dryRun ? '?dry-run=true' : ''
-    }`,
-    {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(mcEmail),
+  const url = `https://api.mailchannels.net/tx/v1/send${
+    email.dryRun ? '?dry-run=true' : ''
+  }`
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(mcEmail),
+  }
+
+  let attempt = 0
+
+  while (attempt < attempts) {
+    const response = await fetch(url, options)
+
+    if (response.ok) {
+      const body = await response.json()
+
+      return {
+        success: true,
+        status: response.status as StatusCode,
+        message: response.statusText,
+        body,
+      }
     }
-  )
 
-  const body = await (response.ok ? response.json() : response.text())
+    attempt++
 
-  return {
-    success: response.ok,
-    status: response.status as StatusCode,
-    message: response.statusText,
-    body,
+    if (attempt < attempts) {
+      await new Promise((resolve) => setTimeout(resolve, delay))
+    } else {
+      const body = await response.text()
+
+      return {
+        success: false,
+        status: response.status as StatusCode,
+        message: response.statusText,
+        body,
+      }
+    }
   }
 }
