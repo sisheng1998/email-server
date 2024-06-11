1a0298608e02440c2ec6580909158f8e8637428d
Sheng <sisheng1998@gmail.com>
2024-06-11 09:13:40 +0800

    switch to nodejs

---
+++ app.cjs
@@ -0,0 +1,8 @@
+// This file is used as entry point to load the app in a CommonJS environment
+// To fix ERR_REQUIRE_ESM error in Node.js
+
+async function loadApp() {
+  await import('./dist/index.js')
+}
+
+loadApp()
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { serve } from '@hono/node-server'
+import 'dotenv/config'
 import { authMiddleware, inputMiddleware } from './middleware'
-import { Bindings } from './types'
 import { Email } from './zod'
 import { sendEmail } from './email'
 import { log, logger } from './logger'
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono()
 
 app.use('*', cors())
 
@@ -18,8 +19,9 @@ app.post('/send', authMiddleware(), inputMiddleware(), async (c) => {
   const email: Email = await c.req.json()
 
   try {
-    const response = await sendEmail(email, c.env)
-    return c.json(response, response.status)
+    // const response = await sendEmail(email, c.env)
+    // return c.json(response, response.status)
+    return c.json({ success: true, message: 'Email sent' })
   } catch (error) {
     const message =
       error instanceof Error
@@ -41,11 +43,23 @@ app.post('/send', authMiddleware(), inputMiddleware(), async (c) => {
 app.notFound((c) => c.json({ success: false, message: 'Not found' }, 404))
 
 app.onError((error, c) => {
-  const message = error.message
+  const message = error.message || 'Unknown error occurred'
 
   log.error(message)
 
   return c.json({ success: false, message }, 500)
 })
 
-export default app
+const port = Number(process.env.PORT || 8787)
+
+serve(
+  {
+    fetch: app.fetch,
+    port,
+  },
+  (info) => {
+    log.info(
+      info.port ? `Server is running on port ${info.port}` : 'Server is running'
+    )
+  }
+)
